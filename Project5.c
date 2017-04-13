fae8251b85938f7c3c5de49c6a0df4c7ead06987
Vanessa Klotzman <vanessa.klotzman@metalab.csun.edu>
2017-04-13 14:53:04 -0700

    it works. add script and it shall be g2g to turn in

--- Project5.c
+++ Project5.c
@@ -1,47 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h> 
 #include <dirent.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
+xs
+void print_dir(char *d);
 
-//determining which files are being passed 
 int main(int argc, char **argv)
 {
+  // One file passed
+  if(argc == 2) {
+    print_dir(argv[1]);
+  }
+  // No files passed
+  else if(argc == 1) {
+    print_dir(".");
+  }
+  else {
+    //passing in way to many files
+    fprintf(stderr, "lsdir: too many files.\n");
+    exit(1);
+  }
+  return 0;
+}
 
-if(argc==2)
+// Recursively prints out directories
+void print_dir(char *d)
 {
-printdirectory(argv[1]);
-}
+  DIR *dp;
+  struct dirent *dir;
+  chdir (d);
+  if ((dp = opendir(".")) == NULL) {
+    fprintf(stderr, "lsdir: cannot open directory %s.\n", d);
+    exit(1);
+  }
+  //looping through the directory 
+  while ((dir = readdir(dp)) != NULL) {
+    if (dir->d_ino == 0) 
+      continue;
+    ///comparing the directory with the directory name 
+    if (strcmp(dir->d_name,".") == 0 || 
+      strcmp(dir->d_name,"..") == 0) continue;   
 
-else if(argc==1) {
-
-printdirectory(".");
-}
-
-else {
-
-fprintf(stderr,"too many files");
-exit(1);
-}
-return 0;
-}
-
-//print the directories out recursively
-void printdirectory(char *d)
-{
-	struct dirent *dir;
-	chdir(d);
-
-	if((dp==open(".") == NULL)) {
-      fprintf(stderr, "cannot open directory", d);
-      exit(1);
-
-	}
-
-	while((dir==readdir(dp))!=NULL) {
-
-		if(dir->)
-	}
+    if (dir->d_type & DT_DIR) {
+      //print out the directory namme 
+      printf("%s\n", dir->d_name);
+      print_dir(dir->d_name);
+      chdir("..");
+    }
+  }  
+  closedir(dp);
 }
 
